bfadd4a9869362a0750cab0354a0f1582438319f
Anna <71184388+annnn-2@users.noreply.github.com>
2024-10-22 18:27:59 +0300

    Add files via upload

--- environment.py
+++ environment.py
@@ -8,6 +8,7 @@ from typing import Callable, List, Literal, Optional, Tuple, Union
 from model.surrogate_model import SurrogateModel
 
 import matplotlib.pyplot as plt
+from scipy.stats import qmc
 
 type Vector = List[float]
 
@@ -26,6 +27,8 @@ class Environment:
         self,
         X: np.ndarray,
         y: np.ndarray,
+        l_bounds: np.ndarray,
+        u_bounds: np.ndarray,
         func: Callable[[np.ndarray, ...], np.ndarray],
         model: Literal["NN", "smth", "smth"] = "NN",
         model_param: dict = None,
@@ -60,6 +63,11 @@ class Environment:
         #extended training set
         self.X = self.X_init
         self.y = self.y_init
+
+        sampler = qmc.LatinHypercube(d=self.X.shape[1])
+        sample = sampler.random(n=1000)
+        sample_scaled = qmc.scale(sample, l_bounds, u_bounds)
+        self.test_set = np.array(sample_scaled)
         
         self.model.fit(self.X_init,self.y_init,first = True)
 
@@ -85,7 +93,8 @@ class Environment:
         self.X = np.concatenate((self.X,[a]),axis=0)
         self.y = np.concatenate((self.y,[self.func(a)]),axis=0)
 
-        _,state = self.model.fit(self.X,self.y)
+        _,model_state = self.model.fit(self.X,self.y)
+        #self.model.fit(self.X,self.y)
 
         if self.cur_iter == self.max_iter:
             self.cur_iter = 0
@@ -96,14 +105,21 @@ class Environment:
 
         self.prev_loss = self.model.loss
 
+
+        state = np.concatenate((model_state, self.model.predict(self.test_set).flatten(), self.X[-10:].flatten()))
+        #print(state)
+        
         return state,-self.model.loss,terminated,truncated
 
     def Reset(self):
 
-        state = self.model.Reset()
+        model_state = self.model.Reset()
+        self.model.Reset()
         self.X = self.X_init
         self.y = self.y_init
 
+        state = np.concatenate((model_state,self.model.predict(self.test_set).flatten(), self.X[-10:].flatten()))
+        
         return state
 
     def toy_Plot(self,x):
@@ -120,13 +136,23 @@ class Environment:
         ax.plot(x, y_pred, label=r"Model $\hat{f}$", lw=2)
 
         ax.plot(
-          self.X,
-          self.y,
+          self.X_init,
+          self.y_init,
           "o",
           color="k",
           markersize=8,
           label="Initial Sample",
         )
+
+        ax.plot(
+          self.X,
+          self.y,
+          "o",
+          color="g",
+          markersize=8,
+          label="New Sample",
+        )
+
 
         ax.set_ylabel(r"$\mathrm{Y}$")
         plt.legend()
